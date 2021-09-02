6ccafde0cf2f0ccc468db5cdaa5b22a3aa49174e
“mk01311” <mk01311@surrey.ac.uk>
2021-09-03 00:34:50 +0100

    Parse location into host and path.

--- server/DeviceDiscovery/Device.js
+++ server/DeviceDiscovery/Device.js
@@ -1,8 +1,26 @@
+let asyncRequest = require('../asyncRequest');
+
 class Device {
-    constructor(uuid, location, type){
+    constructor(uuid, locationUrl, type){
         this.uuid = uuid;
-        this.location = location;
-        this.type = type;
+        this.locationUrl = locationUrl;
+        this.deviceType = type;
+        this.location = this.parseLocationUrl(this.locationUrl);
+        this.requestLocationData();
+    }
+
+    parseLocationUrl = (url) =>{
+        let hostname = url.substring(url.indexOf("//") + 2, url.lastIndexOf(":"));
+        let path = url.slice(url.lastIndexOf("/", url.length));
+        return {hostname: hostname, path: path}
+    }
+
+    requestLocationData = () =>{
+        asyncRequest(this.location.hostname, this.location.path, 443, 'GET', 5000).then((req) => {
+            console.log(req.body)
+        }).catch(() =>{
+
+        })
     }
 
 }
