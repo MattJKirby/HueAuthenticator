c90c7cae1d3c5e8c78a2c2cab3e3803ffe3723ef
“mk01311” <mk01311@surrey.ac.uk>
2021-09-07 00:54:07 +0100

    Missing new kewword in LocationTagDataContainer creation

--- server/DeviceDiscovery/DeviceTypes/HueBridgeDevice.js
+++ server/DeviceDiscovery/DeviceTypes/HueBridgeDevice.js
@@ -6,7 +6,7 @@ class HueBridgeDeviceType extends DeviceType {
         super();
         this.name = "Phillips Hue Bridge";
         this.packetDescriptors = {'hue-bridgeid': null};
-        this.locationTagData = LocationTagDataContainer("friendlyName", "manufacturer", "modelDescription", "modelURL", "modelNumber", "serialNumber")
+        this.locationTagData = new LocationTagDataContainer("friendlyName", "manufacturer", "modelDescription", "modelURL", "modelNumber", "serialNumber")
         this.packetConfig = Object.freeze({target: "phillips_hue:bridge", defaultIp: '239.255.255.250', defaultPort: 1900}) 
     }
 }
